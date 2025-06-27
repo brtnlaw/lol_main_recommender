fix the wrong order dataloader

src/recommender/filters/sgd_collab_filter.py
@@ -29,8 +29,8 @@ class SGDCollabFilter(BaseRecommender):
         model.eval()
         y_pred, y_true = [], []
         with torch.no_grad():
-            for user_ids, champ_ids, ratings in test_loader:
-                preds = model(user_ids, champ_ids)
+            for summoner_ids, champ_ids, ratings in test_loader:
+                preds = model(summoner_ids, champ_ids)
                 y_pred.extend(preds.numpy())
                 y_true.extend(ratings.numpy())
             mse = mean_squared_error(y_true, y_pred)
@@ -60,9 +60,9 @@ class SGDCollabFilter(BaseRecommender):
         for epoch in range(epochs):
             model.train()
             total_loss = 0
-            for user_ids, champ_ids, ratings in train_loader:
+            for summoner_ids, champ_ids, ratings in train_loader:
                 optimizer.zero_grad()
-                preds = model(user_ids, champ_ids)
+                preds = model(summoner_ids, champ_ids)
                 loss = criterion(preds, ratings)
                 loss.backward()
                 optimizer.step()

src/recommender/filters/two_tower.py
@@ -34,10 +34,11 @@ class ChampTower(nn.Module):
         super().__init__()
         self.num_factors = 10
         # not lanes, idk how many
-        self.role_factors = nn.Embedding(5, 8)
         self.attack_type_factors = nn.Embedding(2, 8)
+        self.adaptive_type_factors = nn.Embedding(2, 8)
         self.input_dim = (
-            self.role_factors.embedding_dim + self.attack_type_factors.embedding_dim
+            self.attack_type_factors.embedding_dim
+            + self.adaptive_type_factors.embedding_dim
         )
         self.mlp = nn.Sequential(
             nn.Linear(self.input_dim, 64),
@@ -47,9 +48,12 @@ class ChampTower(nn.Module):
             nn.Linear(32, self.num_factors),
         )
 
-    def forward(self, role_ids, attack_type_ids):
+    def forward(self, attack_type_ids, adaptive_type_ids):
         x = torch.concat(
-            [self.role_factors(role_ids), self.attack_type_factors(attack_type_ids)],
+            [
+                self.attack_type_factors(attack_type_ids),
+                self.adaptive_type_factors(adaptive_type_ids),
+            ],
             dim=-1,
         )
         return self.mlp(x)

src/recommender/utils/data_utils.py
@@ -20,14 +20,16 @@ class ChampionsDataset(Dataset):
         return self.puuids[idx], self.champions[idx], self.ratings[idx]
 
 
-class TwoTowerChampionsDataset(Dataset):
+class ChampionsFeaturesDataset(Dataset):
     """Custom Dataset for two-towers."""
 
     def __init__(self, df):
-        self.ranks = torch.tensor(df["user_id"].values, dtype=torch.long)
-        self.lanes = torch.tensor(df["user_id"].values, dtype=torch.long)
-        self.roles = torch.tensor(df["user_id"].values, dtype=torch.long)
-        self.attack_types = torch.tensor(df["user_id"].values, dtype=torch.long)
+        self.ranks = torch.tensor(df["summoner_rank"].values, dtype=torch.long)
+        self.lanes = torch.tensor(df["summoner_lane"].values, dtype=torch.long)
+        self.roles = torch.tensor(df["champ_attack_type"].values, dtype=torch.long)
+        self.attack_types = torch.tensor(
+            df["champ_adaptive_type"].values, dtype=torch.long
+        )
         self.ratings = torch.tensor(df["rating"].values, dtype=torch.float)
 
     def __len__(self):